e253806ae55bc6c8a886342e925f957a8b7ec6f3
khadziq059 <143367190+khadziq059@users.noreply.github.com>
2024-11-13 15:00:26 +0700

    first commit

---
+++ src/utils/firebase.ts
@@ -0,0 +1,18 @@
+// src/utils/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDE0aNxRELfuTf_jgf2SK4MAuS61RfHonw",
+    authDomain: "vue-firebase-h1d022059.firebaseapp.com",
+    projectId: "vue-firebase-h1d022059",
+    storageBucket: "vue-firebase-h1d022059.firebasestorage.app",
+    messagingSenderId: "832114129533",
+    appId: "1:832114129533:web:42e2560bc46e2af0e04f14"
+};
+
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
